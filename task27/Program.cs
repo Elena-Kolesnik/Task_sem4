f0cf0adf30c433a0f8e6b264e6175e7e36a7faf8
Elena <lenagalkovskaya@yandex.ru>
2022-11-28 14:49:56 +0300

    commit task27

---
+++ task27/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+using static System.Console;
+Clear();
+
+int number = int.Parse(ReadLine());
+
+int result = number.ToString().Sum(c => c - '0');
+
+WriteLine($"{result}");
