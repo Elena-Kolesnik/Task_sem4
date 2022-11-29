51ad2724384fa1e110f4f4e25580a39c37ae8e1f
Elena <lenagalkovskaya@yandex.ru>
2022-11-29 12:13:24 +0300

    commit task29

---
+++ task29/Program.cs
@@ -0,0 +1,27 @@
+﻿using static System.Console;
+Clear();
+
+WriteLine("Введите количество эл-тов в массиве: ");
+int N = Int32.Parse(ReadLine());
+
+FillArray(N);
+
+
+void FillArray(int collection)
+{
+    int [] length = new int [collection];
+
+    for (int i = 0; i < collection; i ++)
+    {
+        
+        length[i] = new Random().Next(-100,100); // Заполняем массив рандомными числами
+    }
+    Write("[");
+    for (int k = 0; k < collection; k ++ )
+    {
+        Write(length[k]);
+        if (k < length.Length - 1) Write(", ");   
+    }
+    WriteLine("]");
+}
+
